223dc546c47fac0d6e4d0fbc1efd7e245035a59e
Your Name <you@example.com>
2026-09-13 07:17:34 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -49,7 +49,15 @@ int is_equal(void* key1, void* key2){
 
 HashMap * createMap(long capacity) {
 
-    return NULL;
+    HashMap *map = (HashMap *)malloc(sizeof(HashMap));
+
+    map->buckets = (Pair **)calloc(capacity, sizeof(Pair *));
+
+    map->size = 0;
+    map->capacity = capacity;
+    map->current = -1;
+
+    return map;
 }
 
 
