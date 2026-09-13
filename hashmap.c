e498f3d66025da749fb835edf63745407abd1705
Your Name <you@example.com>
2026-09-13 07:22:03 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -145,6 +145,14 @@ Pair * searchMap(HashMap * map,  char * key)
 
 void eraseMap(HashMap * map,  char * key) {
 
+    Pair *par = searchMap(map, key);
+
+    if (par != NULL) 
+    {
+        par->key = NULL;
+        map->size--;
+    }
+
 
 }
 
